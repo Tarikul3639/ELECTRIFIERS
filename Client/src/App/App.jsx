cc67150b83b6945f4270500b46c10175eba0bd94
Tarikul3639 <tarikulislam3639@gmail.com>
2025-02-27 18:08:56 +0600

    Initial commit

---
+++ Client/src/App/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Login from '../Page/Login.jsx';
+function App() {
+
+  return (
+    <>
+      <Login></Login>
+    </>
+  )
+} 
+
+export default App
---
+++ Client/src/Page/Login.jsx
@@ -0,0 +1,11 @@
+import '../assets/Style/Login.css';
+
+
+const login = () => {   
+    return(
+        <div className="body">login</div>
+    )
+}
+
+export default login;
+
