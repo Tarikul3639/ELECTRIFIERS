6982c77e948c0f0d11909582356e4bc3f38fd68d
Tarikul Islam <123243130+Tarikul3639@users.noreply.github.com>
2025-05-21 00:58:24 +0600

    Get add

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const Router = require('./routes/Routes.js');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const Router = require("./routes/Routes.js");
+require("dotenv").config();
 
 const app = express();
-app.use(cors({
-    origin: process.env.FRONTEND_URL || '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 
-app.use(express.json()); 
+app.use(express.json());
 
 // Routes
-app.use('/api', Router);
+app.get("/", (req, res) => {
+  res.json({ message: "API is running..." });
+});
+
+app.use("/api", Router);
 app.use("/", (req, res) => {
-    res.send({
-        status: true,
-        message: "Server is running",
-        version: process.env.VERSION || "1.0.0",
-        error: false,
-    })
+  res.send({
+    status: true,
+    message: "Server is running",
+    version: process.env.VERSION || "1.0.0",
+    error: false,
+  });
 });
 
 module.exports = app;
